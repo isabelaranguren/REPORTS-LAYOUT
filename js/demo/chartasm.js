ec0af5947277da139d3aea8b9ec3371a462ced9e
Corey T. Mayweather <42082720+BeenOnChain@users.noreply.github.com>
2023-01-31 10:54:27 -0600

    CM_BRANCH 0.0.1

--- js/demo/chartasm.js
+++ js/demo/chartasm.js
@@ -297,7 +297,7 @@ var myBarChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return number_format(value);
           }
         },
         gridLines: {
